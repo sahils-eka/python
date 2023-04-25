e4c945928b7381fcff5a8dc4f070dc52cc51b98c
Sahil Singh <sahils.2298@gmail.com>
2023-04-26 00:16:37 +0530

    Add user scopes in user_auth_token.py

--- projects/spoti-pie/user_auth_token.py
+++ projects/spoti-pie/user_auth_token.py
@@ -14,7 +14,7 @@ def user_authorization():
     url = "https://accounts.spotify.com/authorize"
     redirect_uri = "http://localhost:8000/callback"
     state = "".join(random.choice(string.ascii_lowercase) for i in range(11))
-    scope = "user-follow-modify user-library-read"
+    scope = "user-follow-modify user-library-read user-top-read user-read-playback-position user-follow-read playlist-read-private"
     query = f"?client_id={client_id}&response_type=token&redirect_uri={redirect_uri}&state={state}&scope={scope}"
     response = requests.get(url=url+query)
     print(response.url)
