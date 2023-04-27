c9a2ff7e4755f2b7e5fa73e4ca5f33eae87692c9
Sahil Singh <sahils.2298@gmail.com>
2023-04-27 21:59:05 +0530

    [spotipie] Add core/logging.py file

---
+++ projects/spoti-pie/core/logging.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+
+class Logger():
+    
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--log",
+            "--log-level",
+            default="warning",
+            help=(
+                "Provide logging level."
+                "Example --log-level debug, default='warning'"
+            )
+        )
+        options = parser.parse_args()
+        levels = {
+            'critical': logging.CRITICAL,
+            'error': logging.ERROR,
+            'warn': logging.WARNING,
+            'warning': logging.WARNING,
+            'info': logging.INFO,
+            'debug': logging.DEBUG
+        }
+        level = levels.get(options.log.lower())
+        if level is None:
+            raise ValueError(
+                f"log level given: {options.log}"
+                f" -- must be one of: {' | '.join(levels.keys())}")
+        self.logger = logging.getLogger("spotipie")
+        self.logger.setLevel(level)
+        consoleHandler = logging.StreamHandler()
+        consoleHandler.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - [%(name)s] [%(levelname)s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S%p')
+        consoleHandler.setFormatter(formatter)
+        self.logger.addHandler(consoleHandler)
